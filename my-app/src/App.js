2c5aa41ffd0ee813c4344ff432900408607cd920
Ali Ulgr <aliulger15@gmail.com>
2020-05-31 01:01:37 +0300

    first react project

---
+++ my-app/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState, useCallback } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
+import Updatedplace from "./places/pages/Updatedplace";
+import Auth from "./user/pages/Auth";
+import { AuthContext } from "./shared/context/auth-context";
+
+const App = () => {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+  const login = useCallback(() => {
+    setisLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setisLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <Updatedplace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
+  );
+};
+
+export default App;
---
+++ my-app/src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+import "./MainNavigation.css";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDriwer = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDriwer = () => {
+    setDrawerIsOpen(false);
+  };
+  return (
+    <>
+      {drawerIsOpen && <Backdrop onClick={closeDriwer} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDriwer}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDriwer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
+  );
+};
+
+export default MainNavigation;
